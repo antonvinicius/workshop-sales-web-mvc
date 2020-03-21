a571ca00256e956876c54b12c8679d8ec00cfe5d
antonvinicius <antoniovrdacosta@gmail.com>
2020-03-21 14:11:31 -0300

    Adding validation

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,19 +9,32 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "{0} size should be between {2} and {1}")]
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Enter a valid {0}")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.00,50000.00,ErrorMessage ="{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
+        
         public Department Department { get; set; }
+        
         public int DepartmentId { get; set; }
+        
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
@@ -50,7 +63,7 @@ namespace SalesWebMvc.Models
 
         public double TotalSales(DateTime initial, DateTime final)
         {
-            return Sales.Where(e => e.Date >= initial && e.Date <= final).Sum(e => e.Amount); 
+            return Sales.Where(e => e.Date >= initial && e.Date <= final).Sum(e => e.Amount);
         }
     }
 }
