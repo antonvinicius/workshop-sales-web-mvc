047d1a86f0e43865422065fe1cb6dc94340ac3ff
antonvinicius <antoniovrdacosta@gmail.com>
2020-03-22 17:03:57 -0300

    adding integrity exception

--- Controllers/SellersController.cs
+++ Controllers/SellersController.cs
@@ -70,8 +70,15 @@ namespace SalesWebMvc.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
         }
 
         public async Task<IActionResult> Details(int? id)
@@ -144,7 +151,7 @@ namespace SalesWebMvc.Controllers
         public IActionResult Error(string message)
         {
             var viewModel = new ErrorViewModel { Message = message, RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
-            
+
             return View(viewModel);
         }
     }
